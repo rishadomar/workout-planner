2265a3a3168cdf75f00a7714d83f0e1f53ccad05
Rishad Omar <rishadomar@gmail.com>
2020-03-16 20:23:32 +0200

    Save secrets in a file that's not on git

--- src/main.js
+++ src/main.js
@@ -7,16 +7,9 @@ import * as firebase from "firebase";
 
 Vue.config.productionTip = false
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDFpBE0EnOLbwVtRis9TCmIyMW1DPgPlio",
-  authDomain: "spinning-a506b.firebaseapp.com",
-  databaseURL: "https://spinning-a506b.firebaseio.com",
-  projectId: "spinning-a506b",
-  storageBucket: "spinning-a506b.appspot.com",
-  messagingSenderId: "918112245605",
-  appId: "1:918112245605:web:4bb3deddf95486f60fb38c",
-  measurementId: "G-WSVDXFB3ZL"
-};
+import firebaseConfig from '@/firebaseConfig';
+
+console.log('firebase config = ', firebaseConfig);
 
 firebase.initializeApp(firebaseConfig)
 
